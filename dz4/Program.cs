f0bd9b9dcf9de5b259909c1fcb807ee15b3fe07b
Nata <Beloborodova.n.s@bk.ru>
2022-07-07 23:30:38 +0300

    dz 4

---
+++ dz4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 4: 
+
+int a, b, c; 
+Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
+Console.WriteLine("Введите третье число: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a<b)
+{
+   max = b;
+}
+if (max<c)
+{
+   max = c;
+}
+    Console.Write("max =  ");
+    Console.WriteLine(max);
+
+
